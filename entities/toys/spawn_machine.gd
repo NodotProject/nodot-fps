7227b7b4fe2a16182e981937fcb1d3b196f361d9
Jake Cattrall <krazyjakee@gmail.com>
2023-05-19 10:25:31 +0100

    new spawn machine model

--- entities/toys/spawn_machine.gd
+++ entities/toys/spawn_machine.gd
@@ -1,7 +1,7 @@
 extends Node3D
 
 @export var fgd_model = {
-  "path": "models/spawner/spawner3d_new.obj",
+  "path": "models/spawner/FixdMsh.fbx",
   "scale": 0.01
 }
 
@@ -10,5 +10,5 @@ func _ready():
 	$Spawner3D.connect("spawns_left_updated", update_labels)
 	
 func update_labels(_spawns_left):
-	$SpawnLimit.text = "Current: %s" % ($Spawner3D.spawn_limit - $Spawner3D.spawns_left)
-	$Current.text = "Limit: %s" % $Spawner3D.spawn_limit
+	$Current.text = "Current: %s" % ($Spawner3D.spawn_limit - $Spawner3D.spawns_left)
+	$SpawnLimit.text = "Limit: %s" % $Spawner3D.spawn_limit
